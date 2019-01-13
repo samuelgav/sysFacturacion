36597f0955baaadc6a9b6799efa5284a8eb9ca5e
Administrador <Administrador@EXT-004.hvitarte.gob.pe>
2019-01-13 12:58:51 -0500

    primer commit

---
+++ sysFacturacion/src/main/java/pe/com/sysFacturacion/SysFacturacionApplication.java
@@ -0,0 +1,14 @@
+package pe.com.sysFacturacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysFacturacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysFacturacionApplication.class, args);
+	}
+
+}
+
