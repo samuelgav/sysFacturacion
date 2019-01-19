73a6f55ef89889aa684bc81b92e6739753306291
Administrador <Administrador@EXT-004.hvitarte.gob.pe>
2019-01-18 21:15:54 -0500

    new commit

--- sysFacturacion/src/main/java/pe/com/sysFacturacion/entity/Factura.java
+++ sysFacturacion/src/main/java/pe/com/sysFacturacion/entity/Factura.java
@@ -1,15 +1,20 @@
 package pe.com.sysFacturacion.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -32,6 +37,15 @@ public class Factura implements Serializable{
 	@ManyToOne(fetch=FetchType.LAZY)
 	private Cliente cliente;
 	
+	@OneToMany(fetch=FetchType.LAZY,cascade=CascadeType.ALL)
+	@JoinColumn(name="factura_id")
+	private List<ItemFactura> items;
+	
+	public Factura() {
+		this.items=new ArrayList<ItemFactura>();
+	}
+
+
 	@PrePersist
 	public void prePersist() {
 		createAt=new Date();
@@ -67,8 +81,30 @@ public class Factura implements Serializable{
 	}
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
+	}	
+	
+	public List<ItemFactura> getItems() {
+		return items;
+	}
+
+
+	public void setItems(List<ItemFactura> items) {
+		this.items = items;
+	}
+
+	public void addItemFactura(ItemFactura item) {
+		this.items.add(item);
 	}
 	
+	public Double getTotal() {
+		Double total=0.0;
+		int size=items.size();
+		for (int i = 0; i <size; i++) {
+			total +=items.get(i).calcularImporte();
+		}
+		return total;
+	}
+
 	private static final long serialVersionUID = 1L;
 	
 }
