3d923b5a8e9b26110367f9cad1e81f4fd5eb9150
samuelgav <samuelgavidia@upeu.edu.pe>
2019-02-03 22:10:34 -0500

    nuevos cambios

--- sysFacturacion/src/main/resources/import.sql
+++ sysFacturacion/src/main/resources/import.sql
@@ -1,2 +1,5 @@
 insert into clientes(id,nombre,apellido,email,create_at,foto) values(1,'Samuel','Gavidia','sam@gmail.com','2018-12-23','');
 insert into clientes(id,nombre,apellido,email,create_at,foto) values(2,'Jose','Suca','suca@gmail.com','2018-12-23','');
+
+insert into productos(nombre, precio, create_at) values('Equipo Panasonic', 4000, Now());
+insert into productos(nombre, precio, create_at) values('Sony Full HD', 4500, Now());
